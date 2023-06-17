ae8f89cf13308bb75d9ca006e6a11b01d7fe4243
zai-n <zaiinn440@gmail.com>
2023-06-17 19:55:17 +0500

    Tokenizer

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,23 +1,6 @@
+import logging
 import time
-import os
-import enum
-
-
-import torch 
-from torchtext.data import Dataset, BucketIterator, Field, Example
-from torchtext.data.utils import interleave_keys
-from torchtext import datasets
-import spacy
-
-
-BOS_TOKEN, EOS_TOKEN, PAD_TOKEN, DATA_DIR_PATH =''
-
-
-class DatasetType(enum.Enum):
-    IWSLT = 0,
-    WMT14 = 1
-
-
-class LanguageDirection(enum.Enum):
-    E2G = 0,
-    G2E = 1
+import numpy as np
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import tensorflow_text
