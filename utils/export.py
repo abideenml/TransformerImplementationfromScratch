9f70edfe59074cf845d6b9c652561761dd544f1f
zai-n <zaiinn440@gmail.com>
2023-06-19 09:00:27 +0500

    Exporting

---
+++ utils/export.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from utils.constants import *
+class ExportTranslator(tf.Module):
+  def __init__(self, translator):
+    self.translator = translator
+
+  @tf.function(input_signature=[tf.TensorSpec(shape=[], dtype=tf.string)])
+  def __call__(self, sentence):
+    (result,
+     tokens,
+     attention_weights) = self.translator(sentence, max_length=MAX_TOKENS)
+
+    return result
+  
